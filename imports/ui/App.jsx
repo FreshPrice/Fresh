d4ade6902e9e8656611908770d992c16b902ef42
Nicole Li <nicolehcli@gmail.com>
2019-05-30 22:20:10 -0700

    Add new modal and form features

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Map from "./components/Map.jsx";
 import CardList from "./components/CardList.jsx";
+import FreshModal from "./components/Modal.jsx";
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Map />
     <CardList />
+    <FreshModal />
   </div>
 );
 
--- imports/ui/components/Modal.jsx
+++ imports/ui/components/Modal.jsx
@@ -1,15 +1,72 @@
+import Button from "@material-ui/core/Button";
+import MaterialModal from "@material-ui/core/Modal";
+import { withStyles } from "@material-ui/core/styles";
 import React, { Component } from "react";
+import FreshForm from "./FreshForm.jsx";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
 
 class Modal extends Component {
+  state = {
+    open: false
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
-    // TODO: Create a Form
-    // TODO: Create a Submit Button
+    const { classes } = this.props;
+
     return (
       <div>
-        <h1>Modal</h1>
+        <Button variant="contained" color="secondary" onClick={this.handleOpen}>
+          Add New Post
+        </Button>
+
+        <MaterialModal
+          aria-labelledby="modal-title"
+          aria-describedby="modal-to-add-new-deal"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <div style={getModalStyle()} className={classes.paper}>
+            <FreshForm />
+          </div>
+        </MaterialModal>
       </div>
     );
   }
 }
 
-export default Modal;
+const styles = theme => ({
+  paper: {
+    position: "absolute",
+    width: theme.spacing(50),
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4)
+  }
+});
+
+// We need an intermediary variable for handling the recursive nesting.
+const ModalWrapped = withStyles(styles)(Modal);
+
+export default ModalWrapped;
