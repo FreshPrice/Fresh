3dc44cef35a8f72353f7ec38c5ac40cb85d6d0a3
l1z1b <echoi@alumni.ubc.ca>
2019-06-01 01:30:10 -0700

    added card component

--- imports/ui/components/CardComponent.jsx
+++ imports/ui/components/CardComponent.jsx
@@ -18,6 +18,7 @@ import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+
 const useStyles = makeStyles(theme => ({
     card: {
         maxWidth: 400,
@@ -41,6 +42,7 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+
 function CardComponent() {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
@@ -54,7 +56,7 @@ function CardComponent() {
             <CardHeader
                 avatar={
                     <Avatar aria-label="Recipe" className={classes.avatar}>
-                        R
+                        A
                     </Avatar>
                 }
                 action={
@@ -67,8 +69,9 @@ function CardComponent() {
             />
             <CardMedia
                 className={classes.media}
-                image="/static/images/cards/paella.jpg"
+                image="/static/images/cards/contemplative-reptile.jpg"
                 title="Paella dish"
+                // style={styles.media}
             />
             <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">
@@ -102,29 +105,11 @@ function CardComponent() {
             </CardActions>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <CardContent>
-                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>Location:</Typography>
                     <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                        minutes.
+                        Nearby!
                     </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                        heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                        browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                        and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                        pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                        without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                        medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                        again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don’t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
+
                 </CardContent>
             </Collapse>
         </Card>
