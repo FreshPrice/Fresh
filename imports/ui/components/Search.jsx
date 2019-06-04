8f0c15ca56fa1c177afa48e6c29b7a0b3475c36a
Nicole Li <nicolehcli@gmail.com>
2019-06-03 20:19:12 -0700

    Add search bar (#11)

--- imports/ui/components/Search.jsx
+++ imports/ui/components/Search.jsx
@@ -1,14 +1,72 @@
-import React, { Component } from "react";
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import InputBase from "@material-ui/core/InputBase";
+import { makeStyles } from "@material-ui/core/styles";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import Toolbar from "@material-ui/core/Toolbar";
+import SearchIcon from "@material-ui/icons/Search";
 
-class Search extends Component {
-  render() {
-    return (
-      <div>
-        {/* TODO: Render search bar and functions */}
-        <h1>Search</h1>
-      </div>
-    );
+const Search = () => {
+  const style = useStyles();
+
+  handleReturnKeyPressed = event => {
+    // TODO: Add backend function to search
+    if (event.key === "Enter") {
+      console.log("Search query is happening...");
+    }
+  };
+
+  return (
+    <div className={style.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={style.search}>
+            <div className={style.searchIcon}>
+              <SearchIcon />
+            </div>
+            <div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: style.inputRoot,
+                  input: style.inputInput
+                }}
+                onKeyPress={this.handleReturnKeyPressed}
+              />
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    width: "100%"
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "70%"
+  },
+  inputInput: {
+    width: "100%"
   }
-}
+}));
 
 export default Search;
