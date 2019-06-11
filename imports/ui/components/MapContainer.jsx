dccdbb4e1c6d4f4c1ae3afe4c34e1361a7008df5
l1z1b <echoi@alumni.ubc.ca>
2019-06-10 18:23:29 -0700

    Add markers on the map

--- imports/ui/components/MapContainer.jsx
+++ imports/ui/components/MapContainer.jsx
@@ -1,18 +1,52 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 export class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [
+        { lat: 49.290338, lng: -123.134112 },
+        { latitude: 49.289554, longitude: -123.132621 },
+        { latitude: 49.290338, longitude: -123.134112 },
+        { latitude: 49.290198, longitude: -123.13234 },
+        { latitude: 49.288931, longitude: -123.13791 },
+        { latitude: 49.290254, longitude: -123.132653 },
+        { latitude: 49.286682, longitude: -123.139346 }
+      ]
+    };
+  }
+
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: store.latitude,
+            lng: store.longitude
+          }}
+          onClick={() => console.log("You clicked me!")}
+        />
+      );
+    });
+  };
+
   render() {
     return (
       <div id="MapContainer">
         <Map
           google={this.props.google}
-          zoom={12}
+          zoom={15}
           initialCenter={{
-            lat: 49.246292,
-            lng: -123.116226
+            lat: 49.290338,
+            lng: -123.134112
           }}
-        />
+        >
+          {this.displayMarkers()}
+        </Map>
       </div>
     );
   }
