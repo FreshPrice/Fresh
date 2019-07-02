46d35dc75b833062a30d9ac4e1261d65aa0b6a19
Nicole Li <nicolehcli@gmail.com>
2019-07-01 18:34:55 -0700

    Sort with unit consideration

--- imports/ui/components/CardList.jsx
+++ imports/ui/components/CardList.jsx
@@ -14,15 +14,29 @@ class CardList extends Component {
   }
 
   sortByPricePressed = () => {
-    var items = this.props.items.items;
+    let items = this.props.items.items;
 
     if (items.length > 1) {
       items.sort(function(a, b) {
+        // Function must be inside here or else cannot be used because of scope
+        let convertToKg = unitString => {
+          if (unitString == "per 100g") {
+            return 0.1;
+          } else if (unitString == "per lb") {
+            return 0.4536;
+          } else {
+            return 1;
+          }
+        };
+
+        let aPriceInKg = a.price * convertToKg(a.unit);
+        let bPriceInKg = b.price * convertToKg(b.unit);
+        console.log(aPriceInKg.toFixed(2), bPriceInKg.toFixed(2));
         // If price is the same, sort by rating
-        if (a.price === b.price) {
-          return a.rating - b.rating;
+        if (aPriceInKg.toFixed(2) === bPriceInKg.toFixed(2)) {
+          return b.rating - a.rating;
         } else {
-          return a.price - b.price;
+          return aPriceInKg - bPriceInKg;
         }
       });
 
