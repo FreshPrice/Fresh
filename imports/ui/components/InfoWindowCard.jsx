965e1ba0f941d31226ab8ec96bc7832e34a8b667
Nicole Li <nicolehcli@gmail.com>
2019-07-08 20:17:09 -0700

    WIP

--- imports/ui/components/InfoWindowCard.jsx
+++ imports/ui/components/InfoWindowCard.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 
 class InfoWindowCard extends React.Component {
   render() {
+    const { classes } = this.props;
     const item = this.props.item;
+
     return (
       //TODO: Actually style the card.
-      <div>
-        <div>{item.item}</div>
-        <div>{item.price}</div>
+      <div className={classes.infoWindow}>
+        <div className={classes.mapDetails}>
+          <img className={classes.mapImage} src="/images/Bananas.png" />
+          <br />
+          <b>{item.name}</b>
+          <br />${item.price} {item.unit}
+        </div>
       </div>
     );
   }
 }
 
-export default InfoWindowCard;
+const useStyles = theme => ({
+  infoWindow: {
+    display: "flex",
+    minWidth: "150px"
+  },
+  mapImage: {
+    height: "50px",
+    flex: 1
+  },
+  mapDetails: {
+    flex: 1,
+    alignItem: "center",
+    paddingRight: "10px",
+    whiteSpace: "nowrap"
+  }
+});
+
+const InfoWindowCardWrapped = withStyles(useStyles)(InfoWindowCard);
+
+export default InfoWindowCardWrapped;
