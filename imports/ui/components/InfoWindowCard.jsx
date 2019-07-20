ff46ab3d92be3f584f877960e5a8c408765b65f4
Yao Liu <25490855+YaoLiu93@users.noreply.github.com>
2019-07-20 09:16:55 -0700

    Style infoWindow Card, enable hover over marker to open infoWindow (#67)
    * WIP
    
    * Enable infowindow show on hover
    
    * Style infoWindow card
    
    * Remove autopublish

--- imports/ui/components/InfoWindowCard.jsx
+++ imports/ui/components/InfoWindowCard.jsx
@@ -1,16 +1,57 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 
 class InfoWindowCard extends React.Component {
   render() {
+    const { classes } = this.props;
     const item = this.props.item;
+    const imageSrc = "/images/" + item.name + ".png";
+    const locationName = item.location.address.substr(
+      0,
+      item.location.address.indexOf(",")
+    );
     return (
-      //TODO: Actually style the card.
-      <div>
-        <div>{item.item}</div>
-        <div>{item.price}</div>
+      <div className={classes.infoWindow}>
+        <div className={classes.infoWindowDetails}>
+          <img className={classes.infoWindowImage} src={imageSrc} />
+          <div className={classes.infoWindowText}>
+            <b>{item.name}</b> <br /> ${item.price} {item.unit} <br />
+            {locationName}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default InfoWindowCard;
+const useStyles = theme => ({
+  infoWindow: {
+    display: "flex",
+    minWidth: "120px"
+  },
+  infoWindowImage: {
+    height: "50px",
+    flex: 1
+  },
+  infoWindowDetails: {
+    display: "flex",
+    flex: 1,
+    paddingRight: "10px",
+    paddingBottom: "10px",
+    whiteSpace: "nowrap",
+    alignItems: "center",
+    margin: "auto"
+  },
+  infoWindowText: {
+    textOverflow: "ellipsis",
+    textAlign: "right",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    maxWidth: "120px"
+  }
+});
+
+const InfoWindowCardWrapped = withStyles(useStyles)(InfoWindowCard);
+
+export default InfoWindowCardWrapped;
