944e0014cb4ff3c731561e9e7b8637b243fba484
Yao Liu <io.yao.liu@gmail.com>
2019-07-23 22:27:37 -0700

    Allow users to upload images optionally

--- imports/ui/components/FreshForm.jsx
+++ imports/ui/components/FreshForm.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import { Form, Select, Label, Message } from "semantic-ui-react";
 import SearchBar from "./SearchBar";
 import GeoSuggest from "./GeoSuggest";
@@ -21,14 +20,15 @@ class FreshForm extends Component {
       add: "",
       lat: 0,
       lng: 0,
-      stateError: false
+      stateError: false,
+      imageSrc: ""
     };
   }
 
-  clearInput = () => {
+  handleItemChange = value => {
     this.setState({
-      name: "",
-      price: ""
+      name: value,
+      imageSrc: "/images/" + value + ".png"
     });
   };
 
@@ -45,7 +45,32 @@ class FreshForm extends Component {
   };
   setLatLng = latLng => {
     this.setState({ lat: latLng.lat, lng: latLng.lng });
-    console.log(this.state.lat);
+  };
+
+  handleFileSelect = e => {
+    const reader = new FileReader();
+    if (e.target.files.length > 0) {
+      const image = e.target.files[0];
+      reader.readAsDataURL(image);
+      reader.onload = () => {
+        let result = reader.result;
+        this.setState({
+          imageSrc: result
+        });
+      };
+
+      reader.onerror = error => {
+        console.log(error);
+        this.setState({
+          imageSrc: "/images/" + this.state.name + ".png"
+        });
+      };
+    } else {
+      // Image was selected then unselected, need to revert to base image.
+      this.setState({
+        imageSrc: "/images/" + this.state.name + ".png"
+      });
+    }
   };
 
   handleSubmit = event => {
@@ -57,12 +82,14 @@ class FreshForm extends Component {
 
   addNewItem = () => {
     this.setState({ stateError: false });
+
     let newItem = {
       name: this.state.name,
       price: parseFloat(this.state.price),
       unit: this.state.unit,
       createdAt: new Date(),
       rating: 0,
+      imageSrc: this.state.imageSrc,
       location: {
         address: this.state.add,
         coords: {
@@ -72,7 +99,6 @@ class FreshForm extends Component {
       }
     };
     this.props.addItem(newItem);
-    this.clearInput();
     this.props.closeModalOnSubmit();
   };
 
@@ -105,7 +131,7 @@ class FreshForm extends Component {
             <SearchBar
               allowAddOptions={true}
               placeholder="Choose Item"
-              onValueUpdate={value => this.setState({ name: value })}
+              onValueUpdate={this.handleItemChange}
               onChange={false}
             />
           </div>
@@ -113,31 +139,41 @@ class FreshForm extends Component {
           {/* Price Input */}
           Price
           <div className="price-bar">
-          <Form.Input
-            fluid
-            labelPosition="right"
-            placeholder="Price"
-            action
-            onChange={this.handleChangePrice}
-            value={this.state.price}
-            type="number"
-            min= "0"
-            step= "0.01" 
-          >
-            <Label basic>$</Label>
-            <input />
-            {/* Select Unit Choices */}
-            <Select
-              compact
-              options={options}
-              defaultValue={PER_HUNDRED_GRAMS}
-              onChange={(event, data) => this.handleChangeUnit(data.value)}
-              className="unit-select"
-            />
-          </Form.Input>
+            <Form.Input
+              fluid
+              labelPosition="right"
+              placeholder="Price"
+              action
+              onChange={this.handleChangePrice}
+              value={this.state.price}
+              type="number"
+              min="0"
+              step="0.01"
+            >
+              <Label basic>$</Label>
+              <input />
+              {/* Select Unit Choices */}
+              <Select
+                compact
+                options={options}
+                defaultValue={PER_HUNDRED_GRAMS}
+                onChange={(event, data) => this.handleChangeUnit(data.value)}
+                className="unit-select"
+              />
+            </Form.Input>
           </div>
+          <br />
+          {/* Optional Image Input */}
+          Image - Optional
+          <div className="geo-suggest">
+            <input
+              type="file"
+              accept="image/*"
+              onChange={this.handleFileSelect}
+            />
+          </div>
+          <br />
           {/* Location Input */}
-          <br/>
           Location
           <div className="geo-suggest">
             <GeoSuggest
@@ -146,7 +182,6 @@ class FreshForm extends Component {
               required
             />
           </div>
-          <br />
           <div>
             <div className="submit-button">
               <DialogAction>
@@ -154,7 +189,10 @@ class FreshForm extends Component {
                   color="pink"
                   type="submit"
                   disabled={
-                    !this.state.add || !this.state.price || !this.state.name || (this.state.lat === 0)
+                    !this.state.add ||
+                    !this.state.price ||
+                    !this.state.name ||
+                    this.state.lat === 0
                   }
                 >
                   Submit
